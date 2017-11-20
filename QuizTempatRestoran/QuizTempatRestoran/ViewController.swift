eff5e0911d86976ef67af7f46cbe53a8c9939df1
Djoendhie <ihaandjoe68@Gmail.com>
2017-11-20 11:29:03 +0700

    Restaurant

---
+++ QuizTempatRestoran/QuizTempatRestoran/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizTempatRestoran
+//
+//  Created by SMK IDN MI on 10/13/17.
+//  Copyright © 2017 Be Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
